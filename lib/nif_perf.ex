7d897b604264443e2b8991d0f8c5cd763d4d17a4
Susumu Yamazaki <zacky1972@gmail.com>
2020-09-01 16:28:56 +0900

    add NIFs to NifPerf

--- lib/nif_perf.ex
+++ lib/nif_perf.ex
@@ -1,2 +1,9 @@
 defmodule NifPerf do
+  @on_load :load_nifs
+
+  def load_nifs do
+    :erlang.load_nif(Application.app_dir(:nif_perf, "priv/libnif"), 0)
+  end
+
+  def test(), do: raise "NIF test/0 not implemented"
 end
